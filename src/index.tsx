1d9c71ccb38d85ed4d570a994c2f7fee12fae455
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-12-16 11:22:43 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from 'react-router-dom';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+
+        <Provider store={store}>
+            <HashRouter>
+                <App/>
+            </HashRouter>
+        </Provider>
+
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
