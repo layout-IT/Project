36a34cc4101c0f0624db80879425f17abadf20bf
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-12-17 16:37:35 +0300

    the first step is completed!

--- src/components/pages/ForgotYourPassword/ForgotYourPassword.tsx
+++ src/components/pages/ForgotYourPassword/ForgotYourPassword.tsx
@@ -28,8 +28,8 @@ export const ForgotYourPassword = () => {
         if (Email.length <= 1) {
             setValidEmail('Invalid email address')
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(Email)) {
-            setValidEmail( 'Invalid email address')
-        }else{
+            setValidEmail('Invalid email address')
+        } else {
             dispatch(FordotPasswordTC(Email, from, message))
             dispatch(IsLoginAC(true))
             navigate('/check-email')
@@ -49,7 +49,7 @@ export const ForgotYourPassword = () => {
                             <input onChange={EmailForgotPassword} id={"emailField"} type="email"
                                    className={s.emailField}
                                    placeholder={'Email'}
-                            onClick={()=>removeValidationFieldError() }/>
+                                   onClick={() => removeValidationFieldError()}/>
                         </div>
                     </form>
                     <p className={validEmail.length > 1 ? s.red : s.instructions}>{validEmail.length > 1 ? validEmail : 'Enter your email address and we will send you further instructions'}</p>
