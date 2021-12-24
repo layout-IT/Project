082e1135b520a142591792e6b945c3459e605620
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-12-24 14:10:05 +0300

    media 800

--- src/api/apiPacks.ts
+++ src/api/apiPacks.ts
@@ -2,7 +2,9 @@ import axios, {AxiosResponse} from 'axios'
 import {stringify} from "querystring";
 
 const instance = axios.create({
-    baseURL: "https://neko-back.herokuapp.com/2.0",
+   // baseURL: "https://neko-back.herokuapp.com/2.0",
+   baseURL: " http://localhost:7542/2.0/",
+
     withCredentials: true,
 })
 
